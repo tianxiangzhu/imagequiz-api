49be74c4aa1bfc01259bfda57d1a71af54614b5f
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-11-29 15:02:47 -0700

    .

---
+++ data.js
@@ -0,0 +1,38 @@
+var quizzes = [
+    ['Quiz1',"daffodil.png","1"],
+    ['Quiz2','cherryblossom.png',"2"],
+    ['Quiz3','daisy.jpg',"3"]
+]   
+
+var questions = {
+    Quiz1: 
+        [
+            ["apple.png",["banana","apple","watermelon","dog"],"apple"],
+            ["dog.jpeg",["cat","car","whale","dog"],"dog"],
+            ["cat.jpg",["cat","car","whale","dog"],"cat"],
+            ["car.jpeg",["cat","car","whale","dog"],"car"],
+            ["whale.jpg",["cat","car","whale","dog"],"whale"],
+            ["fish.jpeg",["cat","fish","whale","dog"],"fish"],
+        ],
+    Quiz2:
+        [
+            ["pie.jpeg",["Banana","pie","watermelon","dog"],"pie"],
+            ["pancakes.jpeg",["cat","pancakes","whale","dog"],"pancakes"],
+            ["cat.jpg",["cat","car","whale","dog"],"cat"],
+            ["car.jpeg",["cat","car","whale","dog"],"car"],
+            ["whale.jpg",["cat","car","whale","dog"],"whale"],
+            ["fish.jpeg",["cat","fish","whale","dog"],"fish"],
+        ],
+    Quiz3:
+        [
+            ["pie.jpeg",["Banana","pie","watermelon","dog"],"pie"],
+            ["pancakes.jpeg",["cat","pancakes","whale","dog"],"pancakes"],
+            ["cat.jpg",["cat","car","whale","dog"],"cat"],
+            ["car.jpeg",["cat","car","whale","dog"],"car"],
+            ["whale.jpg",["cat","car","whale","dog"],"whale"],
+            ["fish.jpeg",["cat","fish","whale","dog"],"fish"],
+        ]
+}
+
+module.exports.quizzes = quizzes
+module.exports.questions = questions
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const { response } = require("express")
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT||3000
+const quizzesdata = require("./quizzesdata")
+const scores = []
+console.log("quizzes:",quizzesdata.quizzes)
+console.log("questions:",quizzesdata.questions)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/quizzes', (request, response) => {
+    response.json(quizzesdata.quizzes)
+})
+
+app.get('/quiz/:id',(request, response) => {
+    let found = quizzesdata.questions["Quiz"+request.params.id]
+    response.json(found)
+})
+app.post('/score', (request, response) => {
+    console.log("score:", request.body)
+    scores.push(request.body)
+    console.log("Update scores:", scores)
+    response.json({message: 'The score saved'})
+
+}
+)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
